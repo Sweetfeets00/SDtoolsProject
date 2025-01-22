d7bfd19f32715a148a13f24ea0c85ae0d7520402
deshawn <deshawncouch@yahoo.com>
2025-01-22 14:09:32 -0500

    update jupiter note book, histogram, app file and readme file

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # SDtoolsProject
-# This project will detail the skills and knowledge acquired from completing the Software Development tools course
-# I will develop and deploy a web application to a cloud service that will be accessible to the public
-# In this project I will be working on car sales advertisemnts.
-# The project URL  
+
+## This project will detail the skills and knowledge acquired from completing the Software Development tools course
+## I will develop and deploy a web application to a cloud service that will be accessible to the public
+## In this project I will be working on car sales advertisemnts.
+
+## To access the included app.py navigate to file location and open with Python3. 
+
+#### The project URL  
 https://sdtoolsproject.onrender.com
--- app.py
+++ app.py
@@ -12,43 +12,31 @@ data.info()
 
 data.duplicated().sum()
 
-data.head(50)
-data['model_year'].isna().sum()
 data['is_4wd']= data['is_4wd'].fillna(0)
-data['is_4wd'] = data['is_4wd'].astype('bool')
-data['model_year']= data['model_year'].fillna(0)
-data['model_year']= data['model_year'].astype('int')
-data['odometer']=data['odometer'].fillna(0)
-data['odometer']=data['odometer'].astype('int')
-data['cylinders']=data['cylinders'].fillna(0)   
-data['cylinders']=data['cylinders'].astype('int')   
+data['is_4wd'] = data['is_4wd'].astype('bool')   
 data['manufacturer']=data['model'].str.split().str[0]
-data.sample(50)
+data['paint_color']=data['paint_color'].fillna('unknown')
+data['model_year'] = data['model_year'].fillna(data.groupby(['model'])['model_year'].transform('median')) 
+data['odometer'] = data['odometer'].fillna(data.groupby(['model_year'])['odometer'].transform('median')) 
+data['cylinders'] = data['cylinders'].fillna(data.groupby(['model'])['cylinders'].transform('median'))
+data.sample(10)
 
 # Vehicle listing analysis
 
 st.title("Vehicle Listing Analysis")
-st.sidebar.header("Filter by Manufacturer")
+st.sidebar.header("Filter by Vehicle Type")
 
-# Selectbox to choose vehicle manufacturer
-manufacturers = data['manufacturer'].unique()
-selected_manufacturer = st.sidebar.selectbox("Select Manufacturer", manufacturers)
+# Selectbox to choose vehicle type
+types = data['type'].unique()
+selected_type = st.sidebar.selectbox("Select Vehicle type", types)
 
-# Filter data based on manufacturer
-filtered_data = data[data['manufacturer'] == selected_manufacturer]
+# Filter data based on vehicle type
+filtered_data = data[data['type'] == selected_type]
 
 # Display filtered data
-st.write(f"Showing data for {selected_manufacturer}")
+st.write(f"Showing data for {selected_type}")
 st.write(filtered_data)
 
-# histogram for Days Listed
-st.subheader("Histogram: Days Listed")
-fig, ax = plt.subplots()
-sns.histplot(filtered_data['days_listed'], bins=10, kde=True, ax=ax)
-ax.set_title("Distribution of Days Listed")
-ax.set_xlabel("Days Listed")
-ax.set_ylabel("Frequency")
-st.pyplot(fig)
 
 # histogram for Price
 st.subheader("Histogram: Price")
@@ -59,12 +47,6 @@ ax.set_xlabel("Price ($)")
 ax.set_ylabel("Frequency")
 st.pyplot(fig)
 
-# histogram for Condition
-st.subheader("Histogram: Condition")
-fig, ax = plt.subplots()
-sns.countplot(x='condition', data=filtered_data, ax=ax)
-ax.set_title("Condition Distribution")
-st.pyplot(fig)
 
 # Correlation between condition and Days listed 
 
@@ -96,3 +78,22 @@ ax.set_ylabel("Condition")
 
 # Show the plot in Streamlit
 st.pyplot(fig)
+
+### The goal here is to see how long are cars listed before they are sold
+
+data['days_listed'].describe()
+
+### It looks like most vehicles are sold around roughly 40 days!To sell cars faster the client should look to adjusting prices after the 45 day mark if vehicles have not sold.
+
+# histogram of days listed
+data['days_listed'].hist(bins = [5, 10, 40, 100, 150, 200])
+plt.xlabel('days listed')
+plt.ylabel('count of vehicles')
+plt.title('Days listed before being sold')
+plt.show()
+
+# Group by 'category' and create histograms for 'value'
+data.groupby('condition')['days_listed'].hist(bins=[20,40,60,80,100,120,140,160,180,200],alpha=0.7, legend=True)
+plt.show()
+
+### It also appears that there is a steep decline in excellent condition vehicles as time passes. 

257f92749cf384e8e4d396462b82f099670ebb42
deshawn <deshawncouch@yahoo.com>
2025-01-23 09:30:35 -0500

    modify

--- app.py
+++ app.py
@@ -26,30 +26,30 @@ data['days_listed'].describe()
 ### It looks like most vehicles are sold around roughly 40 days!To sell cars faster the client should look to adjusting prices after the 45 day mark if vehicles have not sold.
 
 # histogram of days listed
-#g = sns.FacetGrid(data, col="condition", col_wrap=3, sharex=True, sharey=True)
-#g.map(plt.hist, "days_listed", bins=[20, 40, 60, 80, 100, 120, 140, 160, 180, 200], alpha=0.7)
-#g.set_axis_labels("Days Listed", "Frequency")
-#g.fig.suptitle("Days Listed by Vehicle Condition", y=1.02)
-#plt.show()
+g = sns.FacetGrid(data, col="condition", col_wrap=3, sharex=True, sharey=True)
+g.map(plt.hist, "days_listed", bins=[20, 40, 60, 80, 100, 120, 140, 160, 180, 200], alpha=0.7)
+g.set_axis_labels("Days Listed", "Frequency")
+g.fig.suptitle("Days Listed by Vehicle Condition", y=1.02)
+plt.show()
 
 # Group by 'category' and create histograms for 'value'
-#data.groupby('condition')['days_listed'].hist(bins=[20,40,60,80,100,120,140,160,180,200],alpha=0.7, legend=True)
-#plt.show()
+data.groupby('condition')['days_listed'].hist(bins=[20,40,60,80,100,120,140,160,180,200],alpha=0.7, legend=True)
+plt.show()
 
 ### It also appears that there is a steep decline in excellent condition vehicles as time passes. 
 
-#fig = px.scatter(
-#    x='price',
-#    y='days_listed',
-#    color='condition',
-#    size='price',  # Size the markers by price
-#    hover_data=['condition'],
-#    title='Scatterplot: Price vs Days Listed by Condition'
-#)
-#fig.update_layout(
-#    xaxis_title='Price ($)',
-#    yaxis_title='Days Listed'
-#)
-#fig.show()
+fig = px.scatter(
+    x='price',
+    y='days_listed',
+    color='condition',
+    size='price',  # Size the markers by price
+    hover_data=['condition'],
+    title='Scatterplot: Price vs Days Listed by Condition'
+)
+fig.update_layout(
+    xaxis_title='Price ($)',
+    yaxis_title='Days Listed'
+)
+fig.show()
 
 ## Based on the above it also appears that cheaper vehicles sell faster and more expensive vehicles still sell in about 40 days regardless of condiiton
